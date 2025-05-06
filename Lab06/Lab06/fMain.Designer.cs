3cc926e5218c80c526a25f5eed014f711d2b12bd
rar1n <bolbat.ilya13@gmail.com>
2025-05-06 19:18:28 +0300

    Завдання
    Завдання

---
+++ Lab06/Lab06/fMain.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Lab06
+{
+    partial class fMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            tbX1 = new TextBox();
+            tbX2 = new TextBox();
+            tbY = new TextBox();
+            btnCalculate = new Button();
+            btnClear = new Button();
+            btnExit = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Змінна X1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 59);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Змінна X2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 85);
+            label3.Name = "label3";
+            label3.Size = new Size(136, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Результат розрахунку Y";
+            // 
+            // tbX1
+            // 
+            tbX1.Location = new Point(201, 24);
+            tbX1.Name = "tbX1";
+            tbX1.Size = new Size(100, 23);
+            tbX1.TabIndex = 3;
+            // 
+            // tbX2
+            // 
+            tbX2.Location = new Point(201, 53);
+            tbX2.Name = "tbX2";
+            tbX2.Size = new Size(100, 23);
+            tbX2.TabIndex = 4;
+            // 
+            // tbY
+            // 
+            tbY.Location = new Point(201, 82);
+            tbY.Name = "tbY";
+            tbY.ReadOnly = true;
+            tbY.Size = new Size(100, 23);
+            tbY.TabIndex = 5;
+            // 
+            // btnCalculate
+            // 
+            btnCalculate.Location = new Point(12, 125);
+            btnCalculate.Name = "btnCalculate";
+            btnCalculate.Size = new Size(101, 23);
+            btnCalculate.TabIndex = 6;
+            btnCalculate.Text = "Обчислити";
+            btnCalculate.UseVisualStyleBackColor = true;
+            btnCalculate.Click += btnCalculate_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(119, 125);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(88, 23);
+            btnClear.TabIndex = 7;
+            btnClear.Text = "Очистити";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(213, 125);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(88, 23);
+            btnExit.TabIndex = 8;
+            btnExit.Text = "Вихід";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // fMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(317, 167);
+            Controls.Add(btnExit);
+            Controls.Add(btnClear);
+            Controls.Add(btnCalculate);
+            Controls.Add(tbY);
+            Controls.Add(tbX2);
+            Controls.Add(tbX1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            MaximizeBox = false;
+            Name = "fMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Лабораторна робота №6";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox tbX1;
+        private TextBox tbX2;
+        private TextBox tbY;
+        private Button btnCalculate;
+        private Button btnClear;
+        private Button btnExit;
+    }
+}
